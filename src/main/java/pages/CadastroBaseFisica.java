d20eab97826629b9e0c263d7363f83357df5224a
Milena Ribeiro Makarkin <106285031+Purplezinha@users.noreply.github.com>
2024-07-11 09:36:03 -0300

    Update CadastroBaseFisica.java

--- src/main/java/pages/CadastroBaseFisica.java
+++ src/main/java/pages/CadastroBaseFisica.java
@@ -8,69 +8,45 @@ import org.openqa.selenium.support.ui.Select;
 
 
 public class CadastroBaseFisica extends BasePage {
+    
+    @FindBy(id = "Adicionar")
+    private WebElement btnAdicionar;
+    @FindBy(id = "Editar")
+    private WebElement btnEditar;
+    @FindBy(id = "Exibir")
+    private WebElement btnExibir;
+    @FindBy(id = "Excluir")
+    private WebElement btnExcluir;
     @FindBy(id = "idBase")
     private WebElement txIdBase;
-    @FindBy(id = "idUsuario")
-    private WebElement txIdUsuario;
-    @FindBy(id = "nomeBase")
-    private WebElement txNomeBase;
-    @FindBy(id = "dataCadastro")
-    private WebElement txDataCadastro;
-    @FindBy(id = "resposavelBase")
-    private WebElement txResposBase;
-    @FindBy(id = "telefoneBase")
-    private WebElement txTelefoneBase;
-    @FindBy(id = "celularBase")
-    private WebElement txCelularBase;
-    @FindBy(id = "emailBase")
-    private WebElement txEmailBase;
-    @FindBy(id = "sim")
-    private WebElement rdSim;
-    @FindBy(id = "nao")
-    private WebElement rdNao;
-    @FindBy(id = "cadastro")
-    private WebElement btnCadastro;
+    @FindBy(id = "ExcluirBase")
+    private WebElement btnExcluirBase;
+
+
+    public CadastroBaseFisica clicarAdicionar() {
+        btnAdicionar.click();
+        return this;
+    }
+    public CadastroBaseFisica clicarEditar() {
+        btnEditar.click();
+        return this;
+    }
+    public CadastroBaseFisica clicarExibir() {
+        btnExibir.click();
+        return this;
+    }
+    public CadastroBaseFisica clicarExcluir() {
+        btnExcluir.click();
+        return this;
+    }
 
     public CadastroBaseFisica informarIdBase (String idBase){
         txIdBase.sendKeys(idBase);
         return this;
     }
-    public CadastroBaseFisica informarIdUsuario (String idUsuario){
-        txIdUsuario.sendKeys(idUsuario);
-        return this;
-    }
-    
-    public CadastroBaseFisica informarNomeBase (String nomeBase){
-        txNomeBase.sendKeys(nomeBase);
-        return this;
-    }
-    public CadastroBaseFisica informarDataCadastro (String dataCadastro) {
-        txDataCadastro.sendKeys(dataCadastro);
-        return this;
-    }
-    public CadastroBaseFisica informarResponsavelBase (String resposanvelBase){
-        txResposBase.sendKeys(responsavelBase);
-        return this;
-    }
-    public CadastroBaseFisica informarTelefoneBase (String telefoneBase) {
-        txTelefoneBase.sendKeys(telefoneBase);
-        return this;
-    }
-    public CadastroBaseFisica informarCelularBase (String celularBase) {
-        txCelularBase.sendKeys(celularBase);
-        return this;
-    }
-    public CadastroBaseFisica informarEmailBase (String emailBase) {
-        txEmailBase.sendKeys(emailBase);
-        return this;
-    }
-    public CadastroBaseFisica selecionarSim() {
-        rdSim.click();
-        return this;
-    }
 
-    public CadastroBaseFisica clicarCadastro() {
-        btnCadastro.click();
+    public CadastroBaseFisica clicarExcluirBase() {
+        btnExcluirBase.click();
         return this;
     }
 
